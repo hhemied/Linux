d179a80e9211eb3813eba1ff7bce09d77646a55e
Hazem Hemied <hazem.hemied@gmail.com>
2018-06-06 14:52:23 +0200

    Create syncEPEL.sh
    rsync the entire EPEL repo for offline testing environment

---
+++ syncEPEL.sh
@@ -0,0 +1,7 @@
+# this process is working for centos7
+
+yum install yum-utils
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+reposync --repoid=epel --download_path=/var/www/html/epel
+restorecon -r /var/www/html
+systemctl restart httpd
